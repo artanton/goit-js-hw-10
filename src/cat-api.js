5c25a9fc0f4af1c01ed3eca1382776b6ba89a839
artanton <kabletrade@gmail.com>
2023-10-02 15:31:37 +0300

    hw10-4

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,34 +1,26 @@
-import {refs} from './index.js';
+import { refs, breedId } from './index.js';
 
 import axios from 'axios';
 
 axios.defaults.headers.common['x-api-key'] =
   'live_ocuj1A4H8ghKfDbxZ2gghlptMTZix8FFA7xiBhJpDsyFKRSAhAAm7ItegZSezajL';
 
-const BASE_URL = 'https://api.thecatapi.com/v1/images/search';
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const COLLECTION_ENDPOINT = '/breeds';
+const IMAGE_ENDPOINT = '/images/search';
 
 export function fetchBreeds() {
-  axios
-    .get(BASE_URL)
-    .then(response => {
-      if (response.status !== 200) {
-        throw new Error(response.status);
-      }
-      return response.data;
-    })
+  return axios.get(`${BASE_URL}${COLLECTION_ENDPOINT}`).then(response => {
+    return response.data;
+  });
+}
 
-    .then(data => {
-        const container = document.createElement('div'); 
-        data.forEach(element => {
-          const option = document.createElement('option');
-          option.value = element.id;
-          option.textContent = element.name;
-          container.appendChild(option); 
-        });
-        refs.select.appendChild(container); 
-       
-      })
-    .catch(error => {
-      console.error(error);
+export function fetchCatByBreed() {
+  return axios
+    .get(`${BASE_URL}${IMAGE_ENDPOINT}?breed_ids=${breedId}`)
+    .then(response => {
+      return response.data;
     });
 }
+
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import { fetchCatByBreed, fetchBreeds } from './cat-api';
 
-
-
 export const refs = {
   select: document.querySelector('.breed-select'),
   loader: document.querySelector('.loader'),
@@ -9,11 +7,59 @@ export const refs = {
   catInfo: document.querySelector('.cat-info'),
 };
 
-// refs.select.addEventListener('input',showResult);
-// console.dir(refs.select); 
+refs.loader.style.display = 'none';
+refs.error.style.display = 'none';
+refs.error.style.color = 'red';
 
+refs.catInfo.style.display = 'flex';
+refs.catInfo.style.gap = '40px';
+refs.catInfo.style.position = 'fixed';
+refs.catInfo.style.top = '100px';
 
-fetchBreeds(refs);
+function makeSelectOptions() {
+  fetchBreeds()
+    .then(data => {
+      const selectMarkup = data
+        .map(breed => {
+          return `<option value=${breed.id}>${breed.name}</option>`;
+        })
+        .join(' ');
+      refs.select.insertAdjacentHTML('beforeend', selectMarkup);
+    })
+    .catch(error => {
+      console.log(error);
+      return (refs.error.style.display = 'block');
+    });
+}
+makeSelectOptions();
 
+refs.select.addEventListener('input', showResult);
 
+export let breedId = '';
 
+function showResult(e) {
+  breedId = e.target.value;
+
+  return fetchCatByBreed(breedId)
+    .then(data => {
+      const imageMarkup = `<img src="${data[0].url}" alt="" width=400>`;
+
+      const breedMarkup = 
+      `<div>
+      <h2>${data.name}</h2>
+      <p>Description: ${data.description}</p>
+      <p>Temperament: ${data.temperament}</p>
+      </div>`;
+      refs.catInfo.innerHTML = imageMarkup + breedMarkup;
+
+      // console.log(data)
+    })
+    .catch(error => {
+      console.log(error);
+      refs.error.style.display = 'block';
+    });
+}
+
+// function createMarkup (){
+
+// }
